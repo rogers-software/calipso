48226e005fa2941c6b05772cd699d91d2d2005e1
Rogelio <rogelio_ortega_santos@hotmail.com>
2025-04-07 21:34:33 -0600

    Ultimo Commit

--- subo.bat
+++ subo.bat
@@ -4,6 +4,6 @@ git push
 export GOOS=linux
 export GOARCH=amd64
 export CGO_ENABLED=0
-go build -tags lambda.norpc -o main.go
+go build -ldflags="-s -w" -o bootstrap main.go
 rm main.zip
 tar -a -cf main.zip main bootstrap
