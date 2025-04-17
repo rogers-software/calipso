075896bb8373df68abd04b92611be05a50f3b554
Rogelio <rogelio_ortega_santos@hotmail.com>
2025-04-17 07:15:36 -0600

    Ultimo Commit

--- database/insertoregistro.go
+++ database/insertoregistro.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"rogers-software/calipso/models"
 )
 
@@ -12,8 +13,10 @@ func InsertoRegistro(db *sql.DB, u models.Usuario) (int, bool, error) {
 	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING id`
 
 	var pk int
+	fmt.Println("Nombre->", u.Nombre)
 	errInsert := db.QueryRow(query, u.Nombre, u.Apellidos, u.FechaNacimiento, u.Email, u.Password, u.Avatar, u.Banner, u.Biografia, u.Ubicacion, u.SitioWeb).Scan(&pk)
 	if errInsert != nil {
+		fmt.Println("Error", err)
 		return 0, false, err
 	}
 
