0cc54f339d91dd3521234e2fc5579bdfbdee486a
Rogelio <rogelio_ortega_santos@hotmail.com>
2025-04-16 22:54:36 -0600

    Ultimo Commit

--- database/insertoregistro.go
+++ database/insertoregistro.go
@@ -8,11 +8,11 @@ import (
 func InsertoRegistro(db *sql.DB, u models.Usuario) (int, bool, error) {
 	var err error
 	u.Password, err = EncriptarPassword(u.Password)
-	query := `INSERT INTO usuarios (nombre, apellidos, fechanacimiento, email, password, avatar, banner, biografia, ubicaion, sitioweb)
+	query := `INSERT INTO usuarios (nombre, apellidos, fechanacimiento, email, password, avatar, banner, biografia, ubicacion, sitioweb)
 	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING id`
 
 	var pk int
-	errInsert := db.QueryRow(query, u.Nombre, u.Apellidos, u.FechaNacimiento, u.Email, u.Password, u.Avatar, u.Banner, u.Biografia, u.SitioWeb).Scan(&pk)
+	errInsert := db.QueryRow(query, u.Nombre, u.Apellidos, u.FechaNacimiento, u.Email, u.Password, u.Avatar, u.Banner, u.Biografia, u.Ubicacion, u.SitioWeb).Scan(&pk)
 	if errInsert != nil {
 		return 0, false, err
 	}
